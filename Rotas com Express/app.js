15720da8101aa9b58ac2b445d8c834834a53beb6
Sabrinna do Nascimento <143205292+sabrinn07@users.noreply.github.com>
2024-12-24 08:47:49 -0300

    Add files via upload

---
+++ Rotas com Express/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/index', (req, res) => {
+
+})
+
+app.get('/index', (req, res) => {
+
+})
+
+app.listen(port, () => {
+console.log(`Example app listening on port ${port}`)
+})
